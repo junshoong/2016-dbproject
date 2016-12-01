b952a1bd57ebcf20e8a8c98de0389408d8399c7b
harvey <vaporize93@gmail.com>
2016-12-02 00:04:12 +0900

    동문검색 개선(핸드폰 번호 검색가능)

--- skhualumni/alumni/views.py
+++ skhualumni/alumni/views.py
@@ -10,7 +10,12 @@ class UserLV(ListView):
 
         q = self.request.GET.get("q")
         if q:
-            return queryset.filter(name__icontains=q)
+            q1 = queryset.filter(name__icontains=q)
+            q2 = queryset.filter(login_id__icontains=q)
+            q2 = q2.exclude(open_login_id=False)
+            queryset = q1 | q2
+
+            return queryset
         return queryset
 
 
