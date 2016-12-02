2c637c60449e2242b10002210ecd3401b85d2239
koda <kdkyung57@gmail.com>
2016-12-02 11:49:41 +0900

    비회원 게시판 접근 제한 - login 페이지 띄움 - post search 제외

--- skhualumni/board/views.py
+++ skhualumni/board/views.py
@@ -3,9 +3,10 @@ from django.views.generic.edit import FormView
 from django.db.models import Q
 from .models import Post, Comment
 from .forms import CommentForm, PostEditForm, PostSearchForm
+from django.contrib.auth.decorators import login_required
 rows_per_page = 2
 
-
+@login_required
 def index(request):
     post_list = Post.objects.all()
     return render(request, 'board/index.html', {
@@ -27,6 +28,7 @@ class post_search(FormView):
 
 
 # 포스트 보기
+@login_required
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'board/post_detail.html', {
@@ -35,6 +37,7 @@ def post_detail(request, pk):
 
 
 # 글 수정
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -51,6 +54,7 @@ def post_edit(request, pk):
 
 
 # 글 삭제
+@login_required
 def post_delete(request, pk):
     post = Post.objects.get(pk=pk)
     post.delete()
@@ -59,6 +63,7 @@ def post_delete(request, pk):
 
 
 # 새글 작성
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostEditForm(request.POST, request.FILES)
@@ -73,6 +78,7 @@ def post_new(request):
 
 
 # 댓글 생성
+@login_required
 def comment_new(request, pk):
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -89,6 +95,7 @@ def comment_new(request, pk):
 
 
 # 댓글 수정
+@login_required
 def comment_edit(request, post_pk, pk):
     comment = Comment.objects.get(pk=pk)
     if request.method == 'POST':
