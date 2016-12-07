971bc9e4fa9addee70d12f6caafe7c456b9e15e4
harvey <vaporize93@gmail.com>
2016-12-07 21:50:10 +0900

    임원 명단 조회 및 검색가능

--- skhualumni/alumni/views.py
+++ skhualumni/alumni/views.py
@@ -2,19 +2,29 @@ from django.views.generic import ListView, DetailView
 from alumni.models import User
 
 
-class UserLV(ListView):
-    model = User
+class UserSearchMixin(object):
 
     def get_queryset(self):
-        queryset = super(UserLV, self).get_queryset()
+        # 부모의 get_queryset으로 부터 queryset을 가져옵니다.
+        queryset = super(UserSearchMixin, self).get_queryset()
 
+        # q라는 파라미터를 가져옵니다.
         q = self.request.GET.get("q")
         if q:
+            # q가 이름에 포함되거나 핸드폰번호를 공개한 사람의 핸드폰 번호에 포함되면 결과를 냅니다.
             q1 = queryset.filter(name__icontains=q)
             q2 = queryset.filter(login_id__icontains=q)
             q2 = q2.exclude(open_login_id=False)
             queryset = q1 | q2
         return queryset
+
+
+class UserLV(UserSearchMixin, ListView):
+    model = User
+
+
+class ExecutiveLV(UserLV):
+    queryset = User.objects.exclude(position='일반회원')
 
 
 class UserDV(DetailView):
