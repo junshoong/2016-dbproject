ff804c734aed2a43e51c80b254abf45616e51fa2
harvey <vaporize93@gmail.com>
2016-12-22 01:35:15 +0900

    게시판 관련 페이지 일부 수정, 게시글 최신순으로 정렬

--- skhualumni/board/models.py
+++ skhualumni/board/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    writer = models.ForeignKey(settings.AUTH_USER_MODEL)
+    writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField(max_length=10000)
     photo = models.ImageField(blank=True, null=True, upload_to='FreeBoard/%Y/%m/%d')
     created_at = models.DateTimeField(auto_now_add=True)
